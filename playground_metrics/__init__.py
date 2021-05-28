509bb2d93ef70ea51b91ef72c1e7c3ca6f17ce6e
Clément Maliet <clement.maliet@magellium.fr>
2021-05-28 11:21:58 +0200

    Add import to module level to expose more API

--- playground_metrics/__init__.py
+++ playground_metrics/__init__.py
@@ -1,3 +1,6 @@
 __version__ = '1.0.0'
 
-from playground_metrics.map_metric import MeanAveragePrecisionMetric
+from .map_metric import MeanAveragePrecisionMetric
+from .utils import get_type_and_convert, convert_to_bounding_box, convert_to_point, convert_to_polygon
+from .match import non_unitary_match, coco_match, xview_match, IntersectionOverUnionMatcher, EuclideanMatcher, \
+    PointInBoxMatcher, ConstantBoxMatcher
